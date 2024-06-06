dba5bf18ad0b4e669be15ef72376c489f3b2db78
Arshlan khan <arshlankhan69@gmail.com>
2024-06-06 23:39:26 +0530

    Add files via upload

--- calculator.cpp
+++ calculator.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     int x,y;
-    int sum;
+    int SUM;
     cout <<"The first no is: ";
     cin >>x;
     cout <<"The second no is: ";
     cin >>y;
-    sum = x+y;
-    cout <<"first no + second no ="<<sum;
+    SUM = x+y;
+    cout <<"first no + second no ="<<SUM;
 
 
 
