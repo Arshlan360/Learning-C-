9771e16b72ad6d7096709e296b29fb5a42034a9c
Arshlan khan <arshlankhan69@gmail.com>
2024-06-06 23:28:12 +0530

    Add files via upload

---
+++ CWH4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int glo = 69;
+
+void kaifi(){
+    cout << glo<< endl;
+}
+void sum(){
+    int glo = 10;
+    cout<< glo<< endl;
+}
+int main(){
+    int glo = 9;
+    glo =99;
+    cout << glo<<"\n";
+sum(); 
+kaifi();
+
+
+    return 0;
+}
